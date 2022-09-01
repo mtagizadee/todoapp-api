747585ee5871e252ff4ee86ad46536dcaedb2b10
mtagizadee <magatagi0@gmail.com>
2022-09-02 02:13:00 +0400

    project was cleaned and initialized

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
+  const PORT: number = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(PORT, () => console.log(`SERVER STARTED ON PORT: ${PORT}`));
 }
 bootstrap();
